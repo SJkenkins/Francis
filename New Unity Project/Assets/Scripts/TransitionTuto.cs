a66fb6576a12576e6a6d84fb9bf0c45faf4e3075
Tom <tmathelin.csi@gmail.com>
2020-03-31 17:39:52 +0200

    Petit Changes

--- New Unity Project/Assets/Scripts/TransitionTuto.cs
+++ New Unity Project/Assets/Scripts/TransitionTuto.cs
@@ -27,8 +27,11 @@ public class TransitionTuto : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // On vérifie si on entre en contact avec le player, puis on lance la fin du niveau
         if (collision.transform.CompareTag("Player"))
         {
+            // La transition est particulière pour le tuto, donc on crée un cas spécifique avec le bool tuto --> on peut créer d'autres bool pour faire d'autres animations
+            //spécifiques si besoin
             if(tuto == true) {
                 Camera.GetComponent<AudioSource>().Stop();
                 Canvas.SetActive(true);
@@ -38,6 +41,7 @@ public class TransitionTuto : MonoBehaviour
             }
             else
             {
+                //L'animation de base, on désactive le player, et on lance le fade
                 Camera.GetComponent<AudioSource>().Stop();
                 Canvas.SetActive(true);
                 Player.SetActive(false);
@@ -49,7 +53,7 @@ public class TransitionTuto : MonoBehaviour
 
         }
     }
-
+    // Une fois la transition "cinématique" terminée, on 
     public void OnFadeComplete()
     {
         Player.SetActive(true);
